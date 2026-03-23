9829930fd0c1f6f3d63cc380674a9b3fb3789914
frossta-dev <robertgaudreau88@gmail.com>
2026-03-23 14:25:13 -0500

    Initial Commit

---
+++ lua/plugins/treesiter.lua
@@ -0,0 +1,35 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    local treesitter = require('nvim-treesitter')
+    treesitter.install({ 
+      'rust',
+      'javascript', 
+      'typescript', 
+      'zig', 
+      'cpp', 
+      'c', 
+      'java', 
+      'kotlin', 
+      'python', 
+      'lua',
+    })
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { '*' },
+      callback = function() 
+        local filetype = vim.bo.filetype
+        if filetype and filetype ~= "" then
+          local success = pcall(function()
+            vim.treesitter.start()
+          end)
+          if not success then
+            return
+          end
+        end
+      end
+    })
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+}
